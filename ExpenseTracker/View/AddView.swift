88541e77209505fae6fcd7b4c86e04c2f8288c58
Andrew Stannard <andrew.stannard@gmail.com>
2023-11-17 13:52:59 +0000

    Added camera sheet

--- ExpenseTracker/View/AddView.swift
+++ ExpenseTracker/View/AddView.swift
@@ -18,37 +18,70 @@ struct AddView: View {
     @State private var expenseDate = Date.now
     @State private var amount = 0.0
     
+    @State private var showPicker = false
+    @State private var image: UIImage?
+    
     let types = ["Day Subsistence", "Night Subsistence", "Fuel", "Motor Milage"]
+    
     
     var body: some View {
         NavigationView {
-            Form {
-                TextField("Name", text: $name)
-                
-                Picker("Type", selection: $type) {
-                    ForEach(types, id: \.self) {
-                        Text($0)
+            VStack {
+                Form {
+                    Section(header: Text("Expense Details")) {
+                        TextField("Name", text: $name)
+                        
+                        Picker("Type", selection: $type) {
+                            ForEach(types, id: \.self) {
+                                Text($0)
+                            }
+                        }
+                        
+                        DatePicker(selection: $expenseDate, in: ...Date.now, displayedComponents: .date) {
+                            Text("Expense Date")
+                        }
+                        
+                        TextField("Amount", value: $amount, format: .currency(code: "GBP"))
+                            .keyboardType(.decimalPad)
                     }
                 }
-                
-                DatePicker(selection: $expenseDate, in: ...Date.now, displayedComponents: .date) {
-                    Text("Expense Date")
-                }
-                
-                TextField("Amount", value: $amount, format: .currency(code: "GBP"))
-                    .keyboardType(.decimalPad)
+                .frame(maxHeight: 250)
+                .navigationTitle("Add new expense")
+                .toolbar {
+                    Button("Save") {
+                        let item = Expense(expenseDate: expenseDate, name: name, type: type, amount: amount)
+                        expenses.items.append(item)
+                        expenses.expenseTotal()
+                        expenses.save()
+                        dismiss()
+                    }
             }
-            .navigationTitle("Add new expense")
-            .toolbar {
-                Button("Save") {
-                    let item = Expense(expenseDate: expenseDate, name: name, type: type, amount: amount)
-                    expenses.items.append(item)
-                    expenses.expenseTotal()
-                    expenses.save()
-                    dismiss()
+                
+                Button {
+                    print("Button tapped")
+                    showPicker.toggle()
+                } label: {
+                    Label("Camera", systemImage: "camera")
+                        .padding()
+                        .frame(height: 45)
+                        .foregroundStyle(.white)
+                        .background(.blue)
+                        .cornerRadius(15)
                 }
+                .padding()
+                
+                Spacer()
+                
+            }
+            .sheet(isPresented: $showPicker) {
+                ImagePicker(sourceType: .camera, selectedImage: $image)
+                    .ignoresSafeArea()
             }
         }
+        
+
+        
+        
     }
     
 }
